88444a07542a9246beb7ae3d874f2725893ef4e0
BunsoMendoza <bunsomendoza@gmail.com>
2020-03-18 20:39:07 -0700

    Add item button adds dummy data dynamically

--- app/src/main/java/com/example/kitchenapp/MainActivity.java
+++ app/src/main/java/com/example/kitchenapp/MainActivity.java
@@ -1,15 +1,24 @@
 package com.example.kitchenapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.LinearLayout.LayoutParams;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener {
 
     private Button addButton;
     private Button calculateButton;
@@ -18,20 +27,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TableLayout mainTable = (TableLayout) findViewById(R.id.table_main);
         addButton = findViewById(R.id.add_button);
+        addButton.setOnClickListener(this);
         calculateButton = findViewById(R.id.calculate_button);
-        TableRow row= new TableRow(this);
-        TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
-        row.setLayoutParams(lp);
 
 
-        addButton.setOnClickListener(new View.OnClickListener() {
+
+        /*addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openAddItemActivity();
             }
-        });
+        });*/
         calculateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -40,6 +47,52 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+    }
+
+
+    public void onClick(View view) {
+        TableLayout tl = (TableLayout) findViewById(R.id.table_main);
+        switch(view.getId()) {
+            case R.id.add_button:
+
+                TableRow tr = new TableRow(this);
+                EditText editText = new EditText(this);
+                TextView tv1 = new TextView(this);
+                TextView tv2 = new TextView(this);
+                TextView tv3 = new TextView(this);
+                format(tv1);
+                format(editText);
+                format(tv2);
+                format(tv3);
+                tv1.setText("Sample Item");
+                tv2.setText("000");
+                tv3.setText("1 bag");
+                tr.addView(editText);
+                tr.addView(tv1);
+                tr.addView(tv2);
+                tr.addView(tv3);
+                tl.addView(tr, new TableLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+                break;
+            /*case R.id.Button02:
+                int childCount = tl.getChildCount();
+                //tl=(TableLayout)findViewById(R.id.TableLayout01);
+                for (int i=0; i<childCount; i++) {
+                    CheckBox checkBox = ((CheckBox)((TableRow)tl.getChildAt(i)).getChildAt(2));
+                    boolean isChecked = checkBox.isChecked();
+                    // TableRow currentRow = (TableRow)tl.getChildAt(i);
+                    // CheckBox checkBox = (CheckBox)currentRow.getChildAt(2);
+                    if(isChecked){
+                        tl.removeView(checkBox);
+                    }
+                }
+                break;*/
+        }
+    }
+    public void format(View view){
+        Resources res = getResources();
+        Drawable textBackground = ResourcesCompat.getDrawable(res, R.drawable.cell_shape, null);
+        view.setPadding(5, 5,5,5);
+        view.setBackground(textBackground);
     }
     public void openAddItemActivity(){
         Intent intent = new Intent(this, AddItemActivity.class);
@@ -50,4 +103,5 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, ResultsActivity.class);
         startActivity(intent);
     }
+
 }
